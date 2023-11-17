f9b5f44375bb1bafaa0bfd25bc9739ed0b94f5e4
sudenurcure <sude.cure@acibademlabmed.com.tr>
2023-11-17 16:00:11 +0300

    more fixes

--- Update/trialdict.py
+++ Update/trialdict.py
@@ -1,179 +1,181 @@
-class HDL():
-    def __init__(self) -> None:
-        pass
+'''
+Dictionary of Refernce Ranges of NMR Lipoproteins.
+'''
 
-    def HDCH(self):
-        self.HDCH.Unit = "mg/dL"
-        self.HDCH.RoM = range(35,97)
-        self.HDCH.Optimal = [">", 50]
-        self.HDCH.Risk = ["<", 50]
-        return 0
-    
-    def HDFC(self):
-        self.HDFC.Unit = "mg/dL"
-        self.HDFC.RoM = range(7,28)
-        self.HDFC.Optimal = None
-        self.HDFC.Risk = None
-        return 0
-    
+#for float referenge values
 
-class IDL():
-    def __init__(self) -> None:
-        pass
+def float_range(start, stop, step):
+    while start < stop:
+        yield float(start)
+        start += step
 
-    def IDAB(self):
-        self.IDAB.Unit = "mg/dL"
-        self.IDAB.RoM = range(2,18)
-        self.IDAB.Optimal = ["<", 4]
-        self.IDAB.Risk = [">", 6]
-        return 0
-    
-    def IDPN(self):
-        self.IDPN.Unit = "nmol/L"
-        self.IDPN.RoM = range(36,317)
-        self.IDPN.Optimal = ["<", 70]
-        self.IDPN.Risk = [">", 100]
-        return 0
-    
-    def IDCH(self):
-        self.IDCH.Unit = "mg/dL"
-        self.IDCH.RoM = range(4,51)
-        self.IDCH.Optimal = None
-        self.IDCH.Risk = None
-        return 0
-    
-    def IDTG(self):
-        self.IDTG.Unit = "mg/dL"
-        self.IDTG.RoM = range(5,101)
-        self.IDTG.Optimal = ["<", 6]
-        self.IDTG.Risk = [">", 10]
-        return 0
 
-    
-class LDL():
-    def __init__(self) -> None:
-        pass
+#classes of tests
 
-    def LDAB(self):
-        self.LDAB.Unit = "mg/dL"
-        self.LDAB.RoM = range(42,142)
-        self.LDAB.Optimal = ["<", 70]
-        self.LDAB.Risk = [">", 100]
-        return 0
-    
-    def LDCH(self):
-        self.LDCH.Unit = "mg/dL"
-        self.LDCH.RoM = range(55,228)
-        self.LDCH.OptimalnCD = ["<", 70]
-        self.LDCH.OptimalCD = ["<", 100]
-        self.LDCH.Risk = [">", 130]
-        return 0
-    
-    def LDFC(self):
-        self.LDFC.Unit = "mg/dL"
-        self.LDFC.RoM = range(17,64)
-        self.LDFC.Optimal = None
-        self.LDFC.Risk = None
-        return 0
-    
-    def LDPN(self):
-        self.LDPN.Unit = "nmol/L"
-        self.LDPN.RoM = range(760,2561)
-        self.LDPN.Optimal = ["<", 1000]
-        self.LDPN.Risk = [">", 1300]
-        return 0
-    
-    def LDTG(self):
-        self.LDTG.Unit = "mg/dL"
-        self.LDTG.RoM = range(12,46)
-        self.LDTG.Optimal = ["<", 24]
-        self.LDTG.Risk = [">", 28]
-        return 0
-    
-    
-class VLDL():
-    def __init__(self) -> None:
-        pass
+class HDL:
+    HDCH = {
+        "Unit": "mg/dL",
+        "RoM": list(range(35, 97)),
+        "Optimal": [">", 50],
+        "Risk": ["<", 50]
+    }
 
-    def VLAB(self):
-        self.VLAB.Unit = "mg/dL"
-        self.VLAB.RoM = range(3,27)
-        self.VLAB.Optimal = ["<", 6]
-        self.VLAB.Risk = [">", 10]
-        return 0
-    
-    def VLPN(self):
-        self.VLPN.Unit = "nmol/L"
-        self.VLPN.RoM = range(50,474)
-        self.VLPN.Optimal = ["<", 120]
-        self.VLPN.Risk = [">", 180]
-        return 0
-    
-    def VLTG(self):
-        self.VLTG.Unit = "mg/dL"
-        self.VLTG.RoM = range(21,337)
-        self.VLTG.Optimal = ["<", 60]
-        self.VLTG.Risk = [">", 90]
-        return 0
-    
-class CALCULATED():
-    def __init__(self) -> None:
-        pass
+    HDFC = {
+        "Unit": "mg/dL",
+        "RoM": list(range(7, 28)),
+        "Optimal": None,
+        "Risk": None
+    }
 
-    def ABA1(self):
-        self.ABA1.Unit = None
-        self.ABA1.RoM = range(0.30,1.71)
-        self.ABA1.Optimal = ["<", 0.6]
-        self.ABA1.Risk = [">", 0.8]
-    
-    def LDHD(self):
-        self.LDHD.Unit = None
-        self.LDHD.RoM = range(0.98,4.08)
-        self.LDHD.Optimal = ["<", 2]
-        self.LDHD.Risk = [">", 5]
-    
-    def LFLC(self):
-        self.LFLC.Unit = None
-        self.LFLC.RoM = None
-        self.LFLC.Optimal = ["<", 2]
-        self.LFLC.Risk = [">=", 5]
-    
-    def HFHC(self):
-        self.HFHC.Unit = None
-        self.HFHC.RoM = None
-        self.HFHC.Optimal = ["<", 0.5]
-        self.HFHC.Risk = [">=", 0.5]
+class IDL:
+    IDAB = {
+        "Unit": "mg/dL",
+        "RoM": list(range(2, 18)),
+        "Optimal": ["<", 4],
+        "Risk": [">", 6]
+    }
 
-class TotalProtein():
-    def __init__(self) -> None:
-        pass
+    IDPN = {
+        "Unit": "nmol/L",
+        "RoM": list(range(36, 317)),
+        "Optimal": ["<", 70],
+        "Risk": [">", 100]
+    }
 
-    def TPTG(self):
-        self.TPTG.Unit = "mg/dL"
-        self.TPTG.RoM = range(53,491)
-        self.TPTG.Optimal = ["<" , 150]
-        self.TPTG.Risk = [">", 150]
+    IDCH = {
+        "Unit": "mg/dL",
+        "RoM": list(range(4, 51)),
+        "Optimal": None,
+        "Risk": None
+    }
 
-    def TPCH(self):
-        self.TPCH.Unit = "mg/dL"
-        self.TPCH.RoM = range(140,341)
-        self.TPCH.Optimal = ["<" , 200]
-        self.TPCH.Risk = [">", 200]
+    IDTG = {
+        "Unit": "mg/dL",
+        "RoM": list(range(5, 101)),
+        "Optimal": ["<", 6],
+        "Risk": [">", 10]
+    }
 
-    def TPA1(self):
-        self.TPAB.Unit = "mg/dL"
-        self.TPAB.RoM = range(112,218)
-        self.TPAB.Optimal = None
-        self.TPAB.Risk = None
+class LDL:
+    LDAB = {
+        "Unit": "mg/dL",
+        "RoM": list(range(42, 142)),
+        "Optimal": ["<", 70],
+        "Risk": [">", 100]
+    }
 
-    def TPA2(self):
-        self.TPAB.Unit = "mg/dL"
-        self.TPAB.RoM = range(24,49)
-        self.TPAB.Optimal = None
-        self.TPAB.Risk = None
+    LDCH = {
+        "Unit": "mg/dL",
+        "RoM": list(range(55, 228)),
+        "OptimalnCD": ["<", 70],
+        "OptimalCD": ["<", 100],
+        "Risk": [">", 130]
+    }
 
-    def TPAB(self):
-        self.TPAB.Unit = "mg/dL"
-        self.TPAB.RoM = range(48,160)
-        self.TPAB.Optimal = None
-        self.TPAB.Risk = None
+    LDFC = {
+        "Unit": "mg/dL",
+        "RoM": list(range(17, 64)),
+        "Optimal": None,
+        "Risk": None
+    }
+
+    LDPN = {
+        "Unit": "nmol/L",
+        "RoM": list(range(760, 2561)),
+        "Optimal": ["<", 1000],
+        "Risk": [">", 1300]
+    }
+
+    LDTG = {
+        "Unit": "mg/dL",
+        "RoM": list(range(12, 46)),
+        "Optimal": ["<", 24],
+        "Risk": [">", 28]
+    }
+
+class VLDL:
+    VLAB = {
+        "Unit": "mg/dL",
+        "RoM": list(range(3, 27)),
+        "Optimal": ["<", 6],
+        "Risk": [">", 10]
+    }
+
+    VLPN = {
+        "Unit": "nmol/L",
+        "RoM": list(range(50, 474)),
+        "Optimal": ["<", 120],
+        "Risk": [">", 180]
+    }
+
+    VLTG = {
+        "Unit": "mg/dL",
+        "RoM": list(range(21, 337)),
+        "Optimal": ["<", 60],
+        "Risk": [">", 90]
+    }
+
+class CALCULATED:
+    ABA1 = {
+        "Unit": None,
+        "RoM": list(float_range(0.30, 1.71, 0.01)),
+        "Optimal": ["<", 0.6],
+        "Risk": [">", 0.8]
+    }
+
+    LDHD = {
+        "Unit": None,
+        "RoM": list(float_range(0.98, 4.08, 0.01)),
+        "Optimal": ["<", 2],
+        "Risk": [">=", 5]
+    }
+
+    LFLC = {
+        "Unit": None,
+        "RoM": None,
+        "Optimal": ["<", 2],
+        "Risk": [">=", 5]
+    }
+
+    HFHC = {
+        "Unit": None,
+        "RoM": None,
+        "Optimal": ["<", 0.5],
+        "Risk": [">=", 0.5]
+    }
+
+class TotalProtein:
+    TPTG = {
+        "Unit": "mg/dL",
+        "RoM": list(range(53, 491)),
+        "Optimal": ["<" , 150],
+        "Risk": [">", 150]
+    }
+
+    TPCH = {
+        "Unit": "mg/dL",
+        "RoM": list(range(140, 341)),
+        "Optimal": ["<" , 200],
+        "Risk": [">", 200]
+    }
+
+    TPA1 = {
+        "Unit": "mg/dL",
+        "RoM": list(range(112, 218)),
+        "Optimal": None,
+        "Risk": None
+    }
+
+    TPA2 = {
+        "Unit": "mg/dL",
+        "RoM": list(range(24, 49)),
+        "Optimal": None,
+        "Risk": None
+    }
+
+    TPAB = {
+        "Unit": "mg/dL",
+        "RoM": list(range(48, 160)),
+        "Optimal": None,
+        "Risk": None
+    }
